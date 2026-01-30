a026b03d296320840942d706f9e6e301a3c723eb
ram <ramireddy251@gmail.com>
2026-01-30 15:55:17 +0530

    added roboshop file

---
+++ roboshop.sh
@@ -0,0 +1,38 @@
+
+
+AMI_ID="ami-0220d79f3f480ecf5"
+SG_ID="sg-0f8e049cbcce85622"
+INSTANCE_TYPE="t3a.micro"
+
+for instance in $@
+do
+
+instance_id = $( aws ec2 run-instances \
+--image-id $AMI_ID \
+--instance-type $INSTANCE_TYPE \
+--security-group-ids $SG_ID \
+--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+--query 'Instances[0].InstanceId.PrivateIpAddress' \
+--output text )
+
+if [ $instance_id == "frontend" ]; then
+IP=$(
+
+    aws ec2 describe-instances \ 
+    --instance-ids $instance_id \ 
+    --query 'Reservations[].Instances[].PublicIpAddress' \ 
+    --output text
+
+)
+else
+ IP=$(
+
+    aws ec2 describe-instances \ 
+    --instance-ids $instance_id \ 
+    --query 'Reservations[].Instances[].PrivateIpAddress' \ 
+    --output text
+
+) 
+fi
+
+done
