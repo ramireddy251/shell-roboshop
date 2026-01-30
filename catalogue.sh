0bda5aa797f26d20bec747e366fbc46f2fb70658
ram <ramireddy251@gmail.com>
2026-01-30 23:18:01 +0530

    added catalogue file

--- catalogue.sh
+++ catalogue.sh
@@ -74,8 +74,16 @@ VALIDATE $? "Copying mongo repo"
 dnf install mongodb-mongosh -y
 VALIDATE $? "Installing MongoDB client"
 
-mongosh --host $MONGODB_HOST </app/db/master-data.js
-VALIDATE $? "Loading data"
+INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js
+    VALIDATE $? "Loading products"
+else
+    echo -e "Products already loaded ... $Y SKIPPING $N"
+fi
+
+systemctl restart catalogue
+VALIDATE $? "Restarting catalogue"
 
 
