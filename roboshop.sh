fb41a6e7b4dfd06ebe6b26b506fd52350faa1109
ram <ramireddy251@gmail.com>
2026-01-30 16:50:13 +0530

    added roboshop file

--- roboshop.sh
+++ roboshop.sh
@@ -5,7 +5,7 @@ AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-0f8e049cbcce85622"
 INSTANCE_TYPE="t3a.micro"
 ZONE_ID="Z00051682TGCBM123FIQG"
-DOMAIN_MANE="ramireccy.co.in"
+DOMAIN_MANE="ramireddy.co.in"
 
 for instance in $@
 do
