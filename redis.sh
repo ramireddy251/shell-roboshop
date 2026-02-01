05da25c26868b825e272a4fc10eb0075429f3839
ram <ramireddy251@gmail.com>
2026-02-01 12:19:44 +0530

    added redis file

---
+++ redis.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+SCRIPT_DIR=$PWD
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf module disable redis -y
+VALIDATE $? "Disable redis"
+
+dnf module enable redis:7 -y
+VALIDATE $? "enable redis 7"
+
+dnf install redis -y 
+VALIDATE $? "Installing nginx"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
+VALIDATE $? "Allowing remote connections"
+
+sed -i 's/^protected-mode yes/protected-mode no/' /etc/redis/redis.conf
+VALIDATE $? "changed protected-mode to no"
+
+systemctl enable redis 
+systemctl start redis 
+VALIDATE $?  "Enabled and started redis service"
