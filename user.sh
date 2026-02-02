98c9c2806ad146b0abdf8fcbbfe36dd701428492
ram <ramireddy251@gmail.com>
2026-02-02 14:36:05 +0530

    added user file

--- user.sh
+++ user.sh
@@ -25,28 +25,28 @@ VALIDATE(){
     fi
 }
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>>$LOGS_FILE
 VALIDATE $? "disable nodejs"
 
-dnf module enable nodejs:20 -y
+dnf module enable nodejs:20 -y &>>$LOGS_FILE
 VALIDATE $? "enable nodejs 20"
 
-dnf install nodejs -y
+dnf install nodejs -y &>>$LOGS_FILE
 VALIDATE $? "Installing nodejs"
 
 
-id roboshop 
+id roboshop &>>$LOGS_FILE
 if [ $? -ne 0 ]; then
-  useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+  useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
   VALIDATE $? "creating roboshop user"
 else
 echo -e "Roboshop user already exists .... $Y SKIPPING $N"
 fi
 
-mkdir -p /app
+mkdir -p /app &>>$LOGS_FILE
 VALIDATE $? "app directory created"
 
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip 
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOGS_FILE
 VALIDATE $? "Downloading user config"
 
 cd /app 
@@ -55,10 +55,10 @@ VALIDATE $? "changed directory to /app"
 rm -rf /app/*
 VALIDATE $? "removing if user config already exists"
 
-unzip /tmp/user.zip
+unzip /tmp/user.zip &>>$LOGS_FILE
 VALIDATE $? "unzip user config"
 
-npm install 
+npm install &>>$LOGS_FILE
 VALIDATE $? "installing npm"
 
 cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service
