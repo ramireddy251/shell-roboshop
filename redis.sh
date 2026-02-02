830e5c37b38a69acf2d232d38e35de0573f3dda8
ram <ramireddy251@gmail.com>
2026-02-02 14:30:06 +0530

    added redis file

--- redis.sh
+++ redis.sh
@@ -25,13 +25,13 @@ VALIDATE(){
     fi
 }
 
-dnf module disable redis -y
+dnf module disable redis -y &>>$LOGS_FILE
 VALIDATE $? "Disable redis"
 
-dnf module enable redis:7 -y
+dnf module enable redis:7 -y &>>$LOGS_FILE
 VALIDATE $? "enable redis 7"
 
-dnf install redis -y 
+dnf install redis -y &>>$LOGS_FILE
 VALIDATE $? "Installing redis"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
