5605a3669c18d7f480a683a7e5b7fb86c1f73311
ram <ramireddy251@gmail.com>
2026-02-04 13:21:56 +0530

    added shipping file

--- shipping.sh
+++ shipping.sh
@@ -69,7 +69,7 @@ VALIDATE $? "Enabled and started shipping service"
 dnf install mysql -y &>>$LOGS_FILE
 VALIDATE $? "Installing mysql client"
 
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities' &>>$LOGS_FILE
 if [ $? -ne 0 ]; then
 
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
